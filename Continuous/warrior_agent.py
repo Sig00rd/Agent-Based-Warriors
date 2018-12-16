fa62d9289b4c2457ad72d57bba2ef8884259bcf8
Sig00rd <hamletolog@gmail.com>
2018-12-16 20:38:24 +0100

    Add idea config to gitignore. Add more configurable parameters. Start implementing Reynold's flocking rules

--- Continuous/warrior_agent.py
+++ Continuous/warrior_agent.py
@@ -9,6 +9,9 @@ class WarriorAgent(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.hp = 10
+        self.ENEMY_SCANNING_RADIUS = simulation_parameters.VISION_RANGE # promień widzenia przeciwników
+        self.FLOCKING_RADIUS = simulation_parameters.FLOCKING_RADIUS # promień widzenia swoich
+        self.SEPARATION_DISTANCE = simulation_parameters.SEPARATION_DISTANCE # promień w którym żołnierz chce być sam
         self.COHERENCE_FACTOR = simulation_parameters.COHERENCE_FACTOR
         self.MATCH_FACTOR = simulation_parameters.MATCH_FACTOR
         self.SEPARATION_FACTOR = simulation_parameters.SEPARATION_FACTOR
@@ -20,6 +23,40 @@ class WarriorAgent(mesa.Agent):
             self.model.schedule.remove(self)
         else:
             self.move()
+
+    def move(self):
+        visible_enemies = self.scan_for_enemies()
+        visible_allies = self.scan_for_allies()
+
+        velocity_vector = self.calculate_move_vector()
+        normalised_velocity_vector = velocity_vector / np.linalg.norm(velocity_vector)
+        end_point = self.pos + normalised_velocity_vector * self.movement_speed
+        self.model.space.move_agent(self, end_point)
+
+        move_vector = (self.coherence_vector(visible_allies) * self.COHERENCE_FACTOR +
+                       self.match_vector(visible_allies) * self.MATCH_FACTOR +
+                       self.separate_vector(visible_allies) * self.SEPARATION_FACTOR +
+                       self.enemy_position_vector(visible_enemies) * self.ENEMY_POSITION_FACTOR)
+
+        return move_vector
+
+    def coherence_vector(self, visible_allies):
+        return np.array([1.0, 1.0])
+
+    def match_vector(self, visible_allies):
+        return np.array([1.0, 1.0])
+
+    def separate_vector(self, visible_allies):
+        return np.array([1.0, 1.0])
+
+    def enemy_position_vector(self, visible_enemies):
+        return np.array([1.0, 1.0])
+
+    def scan_for_allies(self):
+        pass
+
+    def scan_for_enemies(self):
+        pass
 
     # def move(self):
     #     if self.type == 'blue':
@@ -34,29 +71,6 @@ class WarriorAgent(mesa.Agent):
     #             self.model.space.move_agent(self, new_pos)
     #         else:
     #             self.attack()
-    def move(self):
-        velocity_vector = self.calculate_move_vector()
-        end_point = self.pos + velocity_vector * self.movement_speed
-        self.model.space.move_agent(self, end_point)
-
-    def calculate_move_vector(self):
-        move_vector = (self.coherence_vector() * self.COHERENCE_FACTOR +
-                       self.match_vector() * self.MATCH_FACTOR +
-                       self.separate_vector() * self.SEPARATION_FACTOR +
-                       self.enemy_position_vector() * self.ENEMY_POSITION_FACTOR)
-        return move_vector
-
-    def coherence_vector(self):
-        pass
-
-    def match_vector(self):
-        pass
-
-    def separate_vector(self):
-        pass
-
-    def enemy_position_vector(self):
-        pass
 
     # def attack(self):
     #     if self.type == 'blue':
