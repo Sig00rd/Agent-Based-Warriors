2d72ed8a1f9e0fc8675946617ffb907d28618f32
Przemek Tekieli <mars15951@gmail.com>
2019-01-21 23:45:58 +0100

    movement and elite

--- Continuous/battle_model.py
+++ Continuous/battle_model.py
@@ -12,7 +12,7 @@ import warrior_agent
 
 class BattleModel(Model):
     """A model with some number of agents."""
-    def __init__(self, red_col,red_row,red_squad, blue_col,blue_row,blue_squad, red_movement, blue_movement, width, height):
+    def __init__(self, red_col,red_row,red_squad, blue_col,blue_row,blue_squad,blue_agents_elite_squad, red_movement, blue_movement, width, height):
         self.running = True
         self.space = ContinuousSpace(width, height, False)
         self.schedule = RandomActivation(self)
@@ -21,30 +21,52 @@ class BattleModel(Model):
         self.RED_MOVEMENT_SPEED = red_movement
         self.BLUE_MOVEMENT_SPEED = blue_movement
 
-        separation_y = 1.2
+        separation_y = 1.5
         # Find center
-        red_first_y = ((height/2 - (red_squad * red_row/2 * separation_y)) + separation_y/2) - ((red_squad-1) * separation_y/2)
-        blue_first_y = ((height/2 - (blue_squad * blue_row/2 * separation_y)) + separation_y/2) - ((blue_squad-1) * separation_y/2)
+        red_first_y = ((height/2 - (red_squad * red_row/2 * separation_y)) + separation_y/2) - ((red_squad-1) * separation_y*2)
+        blue_first_y = ((height/2 - (blue_squad * blue_row/2 * separation_y)) + separation_y/2) - ((blue_squad-1) * separation_y*2)
         
         # Create agents
-        self.spawner(1.0,red_first_y, 1.2,separation_y, red_col,red_row,red_squad, 'red')
-        self.spawner(width - 1.0,blue_first_y, -1.2,separation_y, blue_col,blue_row,blue_squad, 'blue')
+        self.spawner(15.0,red_first_y, 1.5,separation_y, red_col,red_row,red_squad,0, 'red')
+        self.spawner(width - 15.0,blue_first_y, -1.5,separation_y, blue_col,blue_row,blue_squad,blue_agents_elite_squad, 'blue')
             
-    def spawner(self, first_x, first_y, separation_x, separation_y, cols, rows, squad, type):
+    def spawner(self, first_x, first_y, separation_x, separation_y, cols, rows, squad, elite_squad, type):
         for i in range(cols):
             for j in range(rows * squad):
                 x = first_x + (separation_x * i)
                 y = first_y + (separation_y * j)
                 
                 # Squad separator
-                y = y + separation_y * (floor(j / rows))
+                y = y + (4*separation_y) * (floor(j / rows))
+
+                casual = squad - elite_squad
+                if casual < 0:
+                    casual = 0
                 
-                self.spawn(x,y,type)
+                elite = True
+                if casual > 0:
+                    if squad-1 == floor(j / rows):
+                        elite = False
+                if casual > 1:
+                    if floor(j / rows) == 0:
+                        elite = False
+                if casual > 2:
+                    if squad-2 == floor(j / rows):
+                        elite = False
+                if casual > 3:
+                    if floor(j / rows) == 1:
+                        elite = False
+                if casual == 5:
+                    elite = False                
+                
+                self.spawn(x,y,type,elite)
         
         
-    def spawn(self,x,y,type):
+    def spawn(self,x,y,type,elite):
         if(type == 'red'):
             a = warrior_agent.RedWarrior(self.next_agent_id, self)
+        elif(elite == True):
+            a = warrior_agent.BlueEliteWarrior(self.next_agent_id, self)
         else:
             a = warrior_agent.BlueCommonWarrior(self.next_agent_id, self)
         pos = np.array((x,y))
--- Continuous/server.py
+++ Continuous/server.py
@@ -9,32 +9,40 @@ def agent_portrayal(agent):
     portrayal = {"Shape": "rect",
                  "Filled": "true",
                  "w": 1,
-                 "h": 1}
+                 "h": 1,
+                 "r": 1}
 
     if agent.type == 'red':
         portrayal["Color"] = "red"
         portrayal["Layer"] = 0
     elif agent.type == 'blue':
-        portrayal["Color"] = "blue"
-        portrayal["Layer"] = 0
+        if agent.elite == False:
+            portrayal["Color"] = "blue"
+            portrayal["Layer"] = 0
+        else:
+            portrayal["Shape"] = "circle"
+            portrayal["Color"] = "blue"
+            portrayal["Layer"] = 0
     return portrayal
 
 red_agents_col = UserSettableParameter('slider', "Number of red agents columns", 3, 1, 7, 1)
 red_agents_row = UserSettableParameter('slider', "Number of red agents rows", 5, 1, 7, 1)
-red_agents_squad = UserSettableParameter('slider', "Number of red agents squad", 3, 1, 5, 1)
+red_agents_squad = UserSettableParameter('slider', "Number of red agents squad", 5, 1, 5, 1)
+red_agents_movement = UserSettableParameter('slider', "Red agent movement speed", 0.1, 0.01, 0.3, 0.01)
 blue_agents_col = UserSettableParameter('slider', "Number of blue agents columns", 3, 1, 7, 1)
 blue_agents_row = UserSettableParameter('slider', "Number of blue agents rows", 5, 1, 7, 1)
-blue_agents_squad = UserSettableParameter('slider', "Number of blue agents squad", 3, 1, 5, 1)
-red_agents_movement = UserSettableParameter('slider', "Red agent movement speed", 0.1, 0.01, 0.3, 0.01)
+blue_agents_squad = UserSettableParameter('slider', "Number of blue agents squad", 5, 1, 5, 1)
+blue_agents_elite_squad = UserSettableParameter('slider', "Number of blue agents elite squad", 1, 0, 5, 1)
 blue_agents_movement = UserSettableParameter('slider', "Blue agent movement speed", 0.1, 0.01, 0.3, 0.01)
 
 
-canvas = ContinuousWorld(agent_portrayal, 50, 50, 750, 750)
+canvas = ContinuousWorld(agent_portrayal, 70, 70, 750, 750)
 
 server = ModularServer(BattleModel,
                        [canvas],
                        "Warrior Model",
                        {"red_col": red_agents_col, "red_row": red_agents_row, "red_squad": red_agents_squad,
-					    "blue_col": blue_agents_col, "blue_row": blue_agents_row, "blue_squad": blue_agents_squad,
-						"red_movement": red_agents_movement, "blue_movement": blue_agents_movement,
-						"width": 50.0, "height": 50.0})
+                        "blue_col": blue_agents_col, "blue_row": blue_agents_row, "blue_squad": blue_agents_squad,
+                        "blue_agents_elite_squad": blue_agents_elite_squad, 
+                        "red_movement": red_agents_movement, "blue_movement": blue_agents_movement,
+                        "width": 70.0, "height": 70.0})
